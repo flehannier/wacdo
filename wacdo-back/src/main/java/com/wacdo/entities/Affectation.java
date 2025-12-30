00492a1cfe13c8791ecc77c276e3cbb689299e51
flehannier <f.lehannier@gmail.com>
2025-12-30 21:46:14 +0100

    feat: affectation

--- wacdo-back/src/main/java/com/wacdo/entities/Affectation.java
+++ wacdo-back/src/main/java/com/wacdo/entities/Affectation.java
@@ -9,6 +9,10 @@ import java.time.LocalDate;
 @Data
 @NoArgsConstructor
 @RequiredArgsConstructor
+/*
+@Table(uniqueConstraints = {
+        @UniqueConstraint(name = "collaborateurRestaurantFonctionConstraint", columnNames = { "collaborateur_id", "restaurant_id", "fonction_id" })
+})*/
 public class Affectation {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
