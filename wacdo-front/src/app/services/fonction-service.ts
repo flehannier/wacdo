098495b5717f6003c0049936a868d0baf5901a32
flehannier <f.lehannier@gmail.com>
2026-02-06 12:42:37 +0100

    BUG add collab

--- wacdo-front/src/app/services/fonction-service.ts
+++ wacdo-front/src/app/services/fonction-service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { FonctionModel } from '../models/fonction-model';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { environment } from '../../environments/environment.development';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root',
